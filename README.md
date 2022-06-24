fc4cc398b2aa0ba7907e819538692777337c4b23
programeloy <riveracubae@gmail.com>
2022-06-24 15:01:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bootstrap-5-kqwczp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/bootstrap-5-kqwczp)
---
+++ update/header.php
@@ -0,0 +1,9 @@
+<?
+include("libreria.php");
+$titulo="Mi Web";
+hacer_encabezado($titulo);
+?>
+<body>
+El cuerpo de la página
+</body>
+</html>
